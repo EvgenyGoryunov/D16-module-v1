299c446a2fec8e316f6be9473f2da1e53fb6812d
Evgeny <ges1987@list.ru>
2022-03-16 22:08:50 +0700

    .

---
+++ gamesite/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+# urlpatterns = [
+#
+#     path('main/', include('main.urls')),
+#     path('create/', include('main.urls')),
+#     path('delete/', include('main.urls')),
+#     path('detail/', include('main.urls')),
+#     path('edit/', include('main.urls')),
+#
+# ]
