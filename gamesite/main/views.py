f8c548b4602e8d575c58326058100c95967e014b
Evgeny <ges1987@list.ru>
2022-03-17 15:33:53 +0700

    .

--- gamesite/main/views.py
+++ gamesite/main/views.py
@@ -1,15 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import CreateView
+
+from .forms import PostForm
 
 
 class PostMain(View):
     def get(self, request):
         return HttpResponse(render(request, 'main.html', ))
 
-class PostCreate(View):
-    def get(self, request):
-        return HttpResponse(render(request, 'post_create.html', ))
+
+
+
+class PostCreate(CreateView):
+    template_name = 'post_create.html'
+    form_class = PostForm
+    success_url = '/'
+
+
+    # def get(self, request):
+    #     return HttpResponse(render(request, 'post_create.html', ))
+
+
+
+#
+# class PostCreate(View):
+#     def get(self, request):
+#         return HttpResponse(render(request, 'post_create.html', ))
+#
+
+
+
 
 class PostDelete(View):
     def get(self, request):
